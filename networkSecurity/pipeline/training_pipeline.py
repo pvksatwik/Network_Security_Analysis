7f144c1769eab1300f81bef1583ca0accd5363d1
satwik <pasumarthipsatwik@gmail.com>
2024-12-10 20:53:28 +0530

    data ingestion comp updated

--- networkSecurity/pipeline/training_pipeline.py
+++ networkSecurity/pipeline/training_pipeline.py
@@ -1,3 +1,4 @@
+import logging
 import os, sys
 
 from networkSecurity.components.data_ingestion import DataIngestion
@@ -34,7 +35,12 @@ class TrainingPipeline:
     
     def start_data_ingestion(self):
         try:
-            pass
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config = self.training_pipeline_config)
+            logging.info("Starting Data Ingestion")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data Ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
